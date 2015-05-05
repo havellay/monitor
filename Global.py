70d3160b33bdce084b31dba4aa8e7b9895ac4084
havellay <hari939@gmail.com>
2015-05-06 02:05:34 +0400

    missed global in the last commit; adding it now

---
+++ Global.py
@@ -0,0 +1,17 @@
+import Attribute
+
+globe = None
+
+class Global():
+ attrib_dict = {} 
+ db_obj       = None
+ db           = None
+ db_filename  = 'data/db'
+ temp_db_obj  = None
+ temp_db      = None
+ temp_db_loc  = ':memory:'
+ users        = None
+
+ def __init__(self):
+   self.attrib_dict['RSI'] = Attribute.RSI
+   return None
